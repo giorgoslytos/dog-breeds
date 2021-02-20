0749bcec4052b39926441871d7f0a11408fd765f
giorgoslytos <giorgoslytos@gmail.com>
2021-02-20 12:10:05 +0200

    addition of FormsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,7 +16,7 @@ import { DogFromUrlComponent } from './pages/dog-from-url/dog-from-url.component
 import { SocialModalComponent } from './components/social-modal/social-modal.component';
 import { FavoritesService } from './services/favorites.service';
 import { BreedsPageComponent } from './pages/breeds-page/breeds-page.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { DogApiService } from './services/dog-api.service';
 import { GetBreedNamePipe } from './pipes/get-breed-name.pipe';
 import { ModalComponent } from './components/modal/modal.component';
@@ -41,6 +41,7 @@ import { ModalComponent } from './components/modal/modal.component';
     BrowserAnimationsModule,
     FlexLayoutModule,
     ReactiveFormsModule,
+    FormsModule,
   ],
   providers: [CookieService, FavoritesService, DogApiService],
   bootstrap: [AppComponent],
